78753b8ccf885a83de8fc86c214e66b163873002
neurouz <neurouzmedia@gmail.com>
2021-01-12 16:59:12 +0100

    UI Updates

--- js/ajax.js
+++ js/ajax.js
@@ -1,80 +1,133 @@
-$(function(){
-    var dataDelay = 0;
+$(function () {
+  var dataDelay = 0;
 
-    const api_url = "https://api.github.com/repositories/";
-    const repos = ["306111171", "292325199", "165105438"];
-    const img_path = "./img/repos/";
+  const api_url = "https://api.github.com/repositories/";
+  const repos = ["306111171", "292325199", "165105438"];
+  const img_path = "./img/repos/";
 
-    async function getData(url){
-        var result = await axios.get(url);
-        return result;
+  async function getData(url) {
+    var result = await axios.get(url);
+    return result;
+  }
+
+  function insertElement(elementName, attributes, content) {
+    var element = document.createElement(elementName);
+    if (attributes) {
+      Object.keys(attributes).forEach((key) => {
+        element.setAttribute(key, attributes[key]);
+      });
     }
-
-    function insertElement(elementName, attributes, content){
-        var element = document.createElement(elementName);
-        if(attributes) {
-            Object.keys(attributes).forEach(key => {
-                element.setAttribute(key, attributes[key]);
-            });
-        }
-        if(content){
-            element.innerHTML = content;
-        }
-        return element;
+    if (content) {
+      element.innerHTML = content;
     }
+    return element;
+  }
 
-    repos.forEach(id => {
-        var repo = getData(api_url.concat(id));
-        repo.then(result => {
+  repos.forEach((id) => {
+    var repo = getData(api_url.concat(id));
+    repo.then((result) => {
+      // Fetching data
+      var title = result.data.name;
+      var link = result.data.html_url;
+      var date = result.data.created_at;
+      var localDate = new Date(date.trim()).toLocaleDateString();
+      var image = img_path.concat(result.data.id).concat(".jpg");
+      var description = result.data.description;
+      var date_modified = new Date(
+        result.data.updated_at.trim()
+      ).toLocaleDateString();
 
-            // Fetching data
-            var title = result.data.name;
-            var link = result.data.html_url;
-            var date = result.data.created_at;
-            var localDate = new Date(date.trim()).toLocaleDateString();
-            var image = img_path.concat(result.data.id).concat(".jpg");
-            var description = result.data.description;
-            var date_modified = new Date(result.data.updated_at.trim()).toLocaleDateString();
+      // Creating HTML elements
+      var card = insertElement("div", {
+        class: "project-card",
+        "data-aos": "zoom-out-up",
+        "data-aos-delay": dataDelay,
+      });
+      var p = insertElement("p", { class: "project-title" }, title);
+      var img = insertElement("img", {
+        src: image,
+        class: "project-image",
+        alt: "project-image",
+      });
+      var dateElement = insertElement(
+        "p",
+        { class: "project-date" },
+        "Created: " + localDate
+      );
+      var last_modified = insertElement(
+        "p",
+        { class: "modified-date" },
+        "Modified: " + date_modified
+      );
 
-            // Creating HTML elements
-            var card = insertElement("div", { "class": "project-card", "data-aos": "zoom-out-up", "data-aos-delay": dataDelay });
-            var p = insertElement("p", { class: "project-title"}, title);
-            var img = insertElement("img", { src: image, class: "project-image", alt: "project-image"});
-            var dateElement = insertElement("p", { class: "project-date"}, "Created: " + localDate);
-            var last_modified = insertElement("p", {class: "modified-date"}, "Modified: " + date_modified);
-            
-            var visit = insertElement("a", {
-                class: "project-link",
-                href: link,
-                target: "_blank"
-            }, "View code");
+      var visit = insertElement(
+        "a",
+        {
+          class: "project-link",
+          href: link,
+          target: "_blank",
+        },
+        "View code"
+      );
 
-            var preview = insertElement("a", {
-                class: "project-preview",
-                onclick: title.concat("_Show()")
-            }, "Demo");
+      var preview = insertElement(
+        "a",
+        {
+          class: "project-preview",
+          onclick: title.concat("_Show()"),
+        },
+        "Demo"
+      );
 
+      if (result.data.id == "306111171") {
+        description =
+          description +
+          "<br />Credentials:<br /><b> Admin : AppAdmin1!<br />User : AppUser1!</b>";
+      }
 
-            if(result.data.id == "306111171"){
-                description = description + "<br />Credentials:<br /><b> Admin : AppAdmin1!<br />User : AppUser1!</b>";
-            }
+      var infotext = insertElement("p", { class: "project-info" }, description);
 
-            var infotext = insertElement("p", { class: "project-info" }, description);
-
-
-            // Appending elements in card div
-            card.appendChild(p);
-            card.appendChild(img);
-            card.appendChild(dateElement);
-            card.appendChild(last_modified);
-            card.appendChild(visit);
-            card.appendChild(preview);
-            card.append(infotext);
-
-            // Appending card into main element
-            document.getElementById("projects").appendChild(card);
-
-            dataDelay += 250;
+      // Appending elements in card div
+      card.appendChild(p);
+      card.appendChild(img);
+      card.appendChild(dateElement);
+      card.appendChild(last_modified);
+      card.appendChild(visit);
+      card.appendChild(preview);
+      if (result.data.id == "292325199") {
+        var div = insertElement("div", {
+          style:
+            "display: flex; flex-direction: row; width: 50%; margin: auto; justify-content: space-around;",
         });
+
+        var downloadMobile = insertElement(
+          "a",
+          {
+            class: "btn btn-primary project-preview",
+            style: "width: 48%; font-size: 12px; color: white",
+          },
+          "<i class='bi bi-download'></i>  Android"
+        );
+
+        var downloadDesktop = insertElement(
+          "a",
+          {
+            class: "btn btn-success project-preview",
+            style: "width: 48%; font-size: 12px; color: white",
+          },
+          "<i class='bi bi-download'></i>  Desktop"
+        );
+
+        div.append(downloadMobile);
+        div.append(downloadDesktop);
+        card.append(div);
+      }
+      card.append(infotext);
+
+      // Appending card into main element
+      document.getElementById("projects").appendChild(card);
+
+      dataDelay += 250;
     });
+  });
 });
--- js/projects.js
+++ js/projects.js
@@ -1,21 +1,21 @@
-function OnlineShop_Show(){
-    window.open('http://neurouz.ddns.net:3001/', '_blank');
+function OnlineShop_Show() {
+  window.open("http://neurouz.ddns.net:3001/", "_blank");
 
-    // TODO: Call OnlineShop() function if needed 
+  // TODO: Call OnlineShop() function if needed
 }
 
-function OnlineShop(){
+function OnlineShop() {}
 
+function EToolService_Show() {
+  window.open("http://neurouz.ddns.net:5000/swagger", "_blank");
+
+  //   toastr["info"](
+  //     "You can still view and clone the source code.",
+  //     "Demo is not hosted yet"
+  //   );
+  return;
+
+  // TODO: Call EToolService() function if needed
 }
 
-
-function EToolService_Show(){
-    toastr["info"]("You can still view and clone the source code.", "Demo is not hosted yet");
-    return;
-
-    // TODO: Call EToolService() function if needed 
-}
-
-function EToolService(){
-
-}
+function EToolService() {}
