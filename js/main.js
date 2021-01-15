be3bc4c7f3bb4bcf9578c6367971cd0785a22537
Ajdin Hukara <neurouzmedia@gmail.com>
2021-01-16 00:48:27 +0100

    Removed loading effects

--- js/main.js
+++ js/main.js
@@ -1,18 +1,3 @@
-
-
-$(".content").hide();
-$('body').append('<div style="" id="loadingDiv"><div class="loader">Loading...</div></div>');
-$(window).on('load', function(){
-    setTimeout(removeLoader, 2000); //wait for page load PLUS two seconds.
-});
-function removeLoader(){
-    $(".content").fadeIn(500);
-    $( "#loadingDiv" ).fadeOut(500, function() {
-        // fadeOut complete. Remove the loading div
-        $( "#loadingDiv" ).remove(); //makes page more lightweight 
-    });  
-}
-
 $(function (){
     AOS.init();
 
